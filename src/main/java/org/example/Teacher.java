7be3054d539e92c1ff72699b4540db3a38fead07
thunguy91 <127452505+thunguy91@users.noreply.github.com>
2025-12-17 20:38:08 -0500

    Group Project with Luke, V.G, & Mark

--- src/main/java/org/example/Teacher.java
+++ src/main/java/org/example/Teacher.java
@@ -4,13 +4,38 @@ public class Teacher {
     // You'll need to implement this class:
 
     // Add private fields for name and className
+    private String name;
+    private String className;
 
     // Add a constructor that takes a name and className and initializes the corresponding fields
+    public Teacher( String name, String className){
+        this.name = name;
+        this.className = className;
+    }
 
     // Add getter methods for name and className
+    public String getName(){
+        return this.name;
+    }
+
+    public String getClassName(){
+        return this.className;
+    }
 
     // Add setter methods for name and className
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
     // Add a toString method that returns the teacher's name and className in the following format:
     // name (className)
+
+    public String toString(){
+        return this.name + " (" + this.className + ")";
+    }
 }
